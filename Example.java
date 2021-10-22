1bd328c25ac775159a8f218a31c0c865fb54ef3a
harisha14 <90745566+harisha14@users.noreply.github.com>
2021-10-22 12:33:18 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,11 @@
+class Example
+{
+public static void main(String arg[])
+{
+int num;
+num=100;
+System.out.println("This number is :"+num);
+num=num*2;
+System.out.println("The value of number*2 is:" +num);
+}
+}
